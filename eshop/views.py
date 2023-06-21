b546f41f51a66f753f0e775549627bebbd53a9e5
Stefan <stefanatanasovski04@gmail.com>
2023-06-21 22:24:35 +0200

    Static Templates finished

---
+++ eshop/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def home(request):
+    context = {}
+    return render(request, 'store/home.html', context)
+
+
+def sneakers(request):
+    context = {}
+    return render(request, 'store/sneakers.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
